ca81743028fecb9158386ba1e811deb1d56a542f
Anhelina <krukanhelina06@gmail.com>
2026-03-03 19:45:48 +0100

    refactor stwo-circuit layout

--- stwo-circuit/src/lib.rs
+++ stwo-circuit/src/lib.rs
@@ -3,3 +3,9 @@
 #![feature(array_chunks)]
 
 pub mod blake3;
+mod proof;
+
+pub use blake3::scheduler::compute_commitment_hash;
+pub use proof::{
+    prove_commitment, verify_proof, CommitmentStatement0, ProofData, VerifyError,
+};
